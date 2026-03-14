9efdd0d0adda1c94f281ee313638764c032f8d88
Muhammad Atif khan <22pwbcs0906@uetpeshawar.edu.pk>
2026-03-14 22:49:34 +0500

    folder manage also done

--- component/Header/nav.jsx
+++ component/Header/nav.jsx
@@ -1,11 +1,41 @@
 'use client'
 import axios from 'axios'
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import {toast} from 'react-toastify'
 import { ClipLoader } from "react-spinners";
 const ImportRepo = () => {
 const [repo , setrepo] = useState("")
 const [loading , setloading] = useState(false)
+const fileInputRef = useRef(null)
+
+ const handleClick = () => {
+    fileInputRef.current.click();
+  };
+ const handleFiles = async(e) => {
+     const files = Array.from(e.target.files);
+      const formData = new FormData();
+
+  files.forEach((file) => {
+    formData.append("file", file);
+     setloading(true)
+     // IMPORTANT: key must be "file"
+  });
+    try {
+    const res = await axios.post("/api/folderManage", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    
+    console.log(res.data);
+    toast.success("Files uploaded successfully");
+    setloading(false)
+  } catch (error) {
+    console.log(error);
+    toast.error("Upload failed");
+    setloading(false)
+  }
+  };
 
 const Handlerepo = async(e)=>{
              e.preventDefault()
@@ -77,15 +107,28 @@ const Handlerepo = async(e)=>{
             {loading? <ClipLoader size={20} color='#fff' /> : 'Analyze'}
           </button>
         </div>
-
+     <input
+  type="file"
+  ref={fileInputRef}
+  onChange={handleFiles}
+  className="hidden"
+  webkitdirectory="true"
+  directory=""
+  multiple
+/>
         {/* Drag & Drop */}
-        <div className="border-2 border-dashed border-gray-200 rounded-lg py-12 flex flex-col items-center justify-center text-gray-400 text-sm hover:border-blue-400">
-          <p className="font-medium text-gray-500 ">
+        <div className="border-2 border-dashed border-gray-200 rounded-lg py-12 flex flex-col items-center justify-center text-gray-400 text-sm hover:border-blue-400"  onClick={handleClick}>
+          {loading? <div><p className="font-medium text-green-600 ">
+            Uploading..............
+          </p>
+          <span className="text-gray-400 text-xs mt-1">
+            
+          </span></div>:<div><p className="font-medium text-gray-500 ">
             Drag and drop local folder
           </p>
           <span className="text-gray-400 text-xs mt-1">
             or click to browse
-          </span>
+          </span></div>}
         </div>
 
       </div>
